109844f012b8abe0ed71d8d227332575338ab791
Siyu Yang <siyuyang@SiyudeMacBook-Pro.local>
2020-10-14 15:36:45 +0100

    enter

--- lines_intersection.py
+++ lines_intersection.py
@@ -35,22 +35,37 @@ m2 = -1
 b2 = 2
 
 A = numpy.array([[-m1, 1], [-m2, 1]])
+# Do not write b = numpy.array([b1],[b2])
 b = numpy.array([[b1], [b2]])
 
+# Inverse of each element, THIS IS NOT WE WANT
+Ainv = A**(-1)
+
+# REAL INVERSE MATRIX OF A
 Ainv = numpy.linalg.inv(A)
 
+# NOT Ainv * b
+print(numpy.dot(Ainv, b))
 
-# print(numpy.dot(Ainv, b))
 
+# Plot two lines
+# x1 - 121 variables in range(-2,6) 
 x_1 = numpy.linspace(-2, 6, 121)
+# Calculate correspondent y for each x
 y_1 = m1*x_1 + b1
 
+# Same thing
 x_2 = x_1
 y_2 = m2*x_2 + b2
 
+# Change colour, linewidth, and linestyle of the line
 plt.plot(x_1, y_1, color='magenta', linewidth=3)
 plt.plot(x_2, y_2, linestyle='--')
 
+# Put labels
 plt.legend(['y = 0.5x + 0.5', 'y = -x + 2'])
+# Add grids in the background
 plt.grid()
+
+# REMEMBER TO SHOW
 plt.show()
